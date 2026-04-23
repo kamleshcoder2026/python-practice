7c3770e958ec9e2ac3688077ffe39d2593191f3a
kamlesh kumar <kamlesh.kumarch55@gmail.com>
2026-04-23 13:45:46 +0530

    added day3.py

---
+++ day3.py
@@ -0,0 +1,35 @@
+students = ["Alia", "Sidharth", "Varun", "Vishal", "Farah"]
+
+print("The nominations for the Student of the Year Award are: ")
+
+for name in students:
+    print(name)
+
+s = {1, 2, 3, 4}
+print(s)
+
+#add
+s = {1, 2, 3}
+s.add(4)
+print(s)
+
+#remove
+s = {1, 2, 3}
+s.remove(2)
+print(s)
+
+
+#union 
+a = {1, 2, 3}
+b = {3, 4, 5}
+print(a | b)
+
+
+#intersection
+a = {1, 2, 3}
+b = {2, 3, 4}
+print(a & b)
+
+
+
+
